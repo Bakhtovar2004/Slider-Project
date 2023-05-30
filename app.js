78db0bb245bc6aaee1b96f997666fa00d2f5abf8
Bakhtovar2004 <mamurovbakhtovar721@gmail.com>
2023-05-30 15:58:20 +0500

    Slider-commit

---
+++ app.js
@@ -0,0 +1,46 @@
+let downBtn = document.querySelector('.down-button')
+let upBtn = document.querySelector('.up-button')
+let mainSlide = document.querySelector('.main-slide')
+let sidebar = document.querySelector('.sidebar')
+let container = document.querySelector('.container')
+let slidesCount = mainSlide.querySelectorAll('div').length
+
+let activeSlideIndex = 0
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
+
+upBtn.addEventListener('click',() =>{
+    changeSlide('up')
+})
+
+downBtn.addEventListener('click', () => {
+    changeSlide('down')
+})
+
+document.addEventListener('keydown', (e) => {
+    if(e.key === 'ArrowUp'){
+        changeSlide('up')
+    }else if  (e.key === 'ArrowDown'){
+        changeSlide('down')
+    }
+})
+
+function changeSlide(direction){
+    if(direction === 'up'){
+        activeSlideIndex++
+        if(activeSlideIndex === slidesCount){
+            activeSlideIndex = 0
+        }
+    }else if (direction === 'down'){
+        activeSlideIndex--
+        if(activeSlideIndex < 0){
+            activeSlideIndex = slidesCount -1
+        }
+    }
+    let height = container.clientHeight
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
+}
+
+
+
